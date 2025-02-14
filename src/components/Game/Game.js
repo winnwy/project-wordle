37ad6af4a6b99a4bafb3087289512fa1962d2c86
Winn Z <winnwyz@gmail.com>
2025-02-15 02:53:24 +1100

    Add logic to change slot color

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -28,7 +28,7 @@ function Game() {
 
   return (
     <>
-      <GuessResults guesses={guesses} />
+      <GuessResults guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const guessAnswer = checkGuess(value ? value.guess : undefined, answer);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span
+          key={num}
+          className={
+            "cell " + (guessAnswer ? guessAnswer[num].status : undefined)
+          }
+        >
           {value ? value.guess[num] : undefined}
         </span>
       ))}
